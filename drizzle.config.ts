63c2b7dfb94f81ddd6062662841eee6a200a8d09
muhammadsufiyanbaig <send.sufiyan@gmail.com>
2026-06-22 16:46:56 +0500

    change db varible name

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.NEON_DATABASE_URL!,
   },
   verbose: true,
   strict: true,
